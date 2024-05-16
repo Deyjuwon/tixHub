96d2421e0cfc24ae879419aa09479b591486bb81
Deyjuwon <temitayoadejuwon@gmail.com>
2024-05-16 22:38:10 +0100

    created header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+
+const Header = () => {
+  return (
+    <div className="flex justify-around ">
+        <div>
+            <p>tixhub</p>
+        </div>
+        <nav>
+            <ul>
+                <li>Product</li>
+                <li>Popular</li>
+                <li>Pricing</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <div>
+            <button>Login</button>
+            <button>Sign Up</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
